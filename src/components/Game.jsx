4a5fd75e93fe1923dad21fe87ba4f023b79fd999
Pedro Silva <pedrosilva354@gmail.com>
2024-08-14 22:08:51 -0300

    first commitadoa

---
+++ src/components/Game.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+Game.propTypes = {
+    title: PropTypes.string,
+    cover: PropTypes.string,
+    onRemove: PropTypes.func,
+}
+
+export default function Game({ title, cover, onRemove}){
+    return (
+        <div>
+        <img src={cover} alt="" />
+        <div>
+          <h2>{title}</h2>
+          <button onClick={onRemove}>Remover Jogo</button>
+        </div>
+      </div>
+    )
+}
+//npm i prop-types para o es-lint
